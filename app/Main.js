e027c3eacc0a41d53e292e8d792eb891d66bd69d
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-11-26 22:16:06 +0530

    Worked on components

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function ExampleComponent() {
+// My Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+
+function Main() {
   return (
-    <div>
-      <h1>This is our App!!!</h1>
-      <p>The sky is blue and grass is green.</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
-ReactDOM.render(<ExampleComponent />, document.getElementById("app"));
+ReactDOM.render(<Main />, document.getElementById("app"));
 
 if (module.hot) {
   module.hot.accept();
---
+++ app/components/Container.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+function ComponentName(props) {
+  return (
+    <>
+      <div
+        className={
+          "container py-md-5 " + (props.wide ? "" : "container--narrow")
+        }
+      >
+        {props.children}
+      </div>
+    </>
+  );
+}
+
+export default ComponentName;
