d80d16bc4231c272c349aea7931eed51e5ac3480
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-11-28 16:00:52 +0530

    updated to loggin in feature

---
+++ app/components/HeaderLoggedIn.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+
+function HeaderLoggedIn(props) {
+  function handleLogout() {
+    props.setLoggedIn(false);
+    localStorage.removeItem("complexAppToken");
+    localStorage.removeItem("complexAppUsername");
+    localStorage.removeItem("complexAppAvatar");
+  }
+  return (
+    <div className="flex-row my-3 my-md-0">
+      <a href="#" className="text-white mr-2 header-search-icon">
+        <i className="fas fa-search"></i>
+      </a>
+      <span className="mr-2 header-chat-icon text-white">
+        <i className="fas fa-comment"></i>
+        <span className="chat-count-badge text-white"> </span>
+      </span>
+      <a href="#" className="mr-2">
+        <img
+          className="small-header-avatar"
+          src={localStorage.getItem("complexAppAvatar")}
+        />
+      </a>
+      <a className="btn btn-sm btn-success mr-2" href="/create-post">
+        Create Post
+      </a>
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">
+        Sign Out
+      </button>
+    </div>
+  );
+}
+
+export default HeaderLoggedIn;
